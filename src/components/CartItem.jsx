67ee426c31a46fd4a0c37ba7adf788330c3bf0c7
Ayush Mamgain <ayushmamgain1234@gmail.com>
2024-01-15 23:29:21 +0530

    message

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,6 +1,7 @@
 import { useDispatch } from "react-redux";
 import { removeItem } from "../redux/slices/cartSlice";
 import { FaTrash } from "react-icons/fa";
+import toast from "react-hot-toast";
 
 export default function CartItem({ item }) {
     const dispatch = useDispatch();
@@ -15,7 +16,10 @@ export default function CartItem({ item }) {
                 <p className="cart-item-description">{item.description}</p>
                 <div className="cart-item-footer">
                     <span className="cart-item-price">${item.price}</span>
-                    <FaTrash className="cart-remove-btn" onClick={() => dispatch(removeItem(item.id))} />
+                    <FaTrash className="cart-remove-btn" onClick={() => {
+                        dispatch(removeItem(item.id));
+                        toast.error("Item removed");
+                    }} />
                 </div>
             </div>
         </div>
