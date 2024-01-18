d6e44d29503bc6a81a9076960f2a98093625a2e2
Ayush-Mamgain <ayushmamgain1234@gmail.com>
2024-01-18 16:10:06 +0530

    let user change item count

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,10 +1,25 @@
 import { useDispatch } from "react-redux";
 import { removeItem } from "../redux/slices/cartSlice";
 import { FaTrash } from "react-icons/fa";
-import toast from "react-hot-toast";
+import { useState } from "react";
 
 export default function CartItem({ item }) {
     const dispatch = useDispatch();
+    const [itemCount, setItemCount] = useState(1);
+    const [itemTotalPrice, setItemTotalPrice] = useState(item.price);
+
+    function countHandler(increment) {
+        if(increment) {
+            setItemCount(prevCount => prevCount+1);
+            setItemTotalPrice(prevPrice => prevPrice + item.price);
+        }
+        else {
+            if(itemCount !== 1) {
+                setItemCount(prevCount => prevCount - 1);
+                setItemTotalPrice(prevPrice => prevPrice - item.price);
+            }
+        }
+    }
 
     return (
         <div className="cart-item">
@@ -16,10 +31,14 @@ export default function CartItem({ item }) {
                 <p className="cart-item-description">{item.description}</p>
                 <div className="cart-item-footer">
                     <span className="cart-item-price">${item.price}</span>
-                    <FaTrash className="cart-remove-btn" onClick={() => {
-                        dispatch(removeItem(item.id));
-                        toast.error("Item removed");
-                    }} />
+                    <button className="item-count-btn" onClick={() => countHandler(false)}>
+                        -
+                    </button>
+                    <span>{itemTotalPrice.toFixed(2)}</span>
+                    <button className="item-count-btn" onClick={() => countHandler(true)}>
+                        +
+                    </button>
+                    <FaTrash className="cart-remove-btn" onClick={() => dispatch(removeItem(item.id))} />
                 </div>
             </div>
         </div>
